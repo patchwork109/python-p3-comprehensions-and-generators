9f1cc7e84d8be5bfd9df1ba6fbef3eb7b0f4219f
Samantha Genevay <samanthagenevay@gmail.com>
2023-03-13 14:27:02 -0700

    Completed Lab

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    evens = [num for num in num_list if num % 2 == 0]
+    print(evens)
+    return evens
+
 
 def make_exclamation(sentence_list):
-    pass
+    sentence_with_exclamation = [(sentence + "!") for sentence in sentence_list]
+    print(sentence_with_exclamation)
+    return sentence_with_exclamation
